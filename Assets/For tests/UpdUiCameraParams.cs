dc297e0cf39cc70066b15ebcc14419e1abcf09e8
Levael Savvy <Levael.Savvy@gmail.com>
2023-10-25 21:21:17 +0300

    Add project files.

---
+++ Assets/For tests/UpdUiCameraParams.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class MatchFov : MonoBehaviour
+{
+    public Camera vrCamera; // Перетащите вашу VR-камеру сюда в инспекторе
+    private Camera uiCamera;
+
+    void Start()
+    {
+
+        vrCamera = GameObject.Find("VrHelmetCamera").GetComponent<Camera>();
+        uiCamera = this.GetComponent<Camera>();
+        
+    }
+
+    private void Update()
+    {
+        uiCamera.fieldOfView = vrCamera.fieldOfView;
+    }
+}
